1d51c277eaa9169e1d9e3adf3d19d348020f818b
Lê Trần Phúc Anh IT <174760692+PhucAnhIT@users.noreply.github.com>
2025-12-23 11:05:30 +0700

    Update locket_gold_2025.js

--- locket_gold_2025.js
+++ locket_gold_2025.js
@@ -25,7 +25,7 @@ var locket01 = {
   grace_period_expires_date: null,
   purchase_date: "2025-11-25T00:00:00Z",
   product_identifier: "com.locket02.premium.yearly",
-  expires_date: "2099-23-12T01:04:17Z"
+  expires_date: "2099-12-18T01:04:17Z"
 };
 
 const match = Object.keys(mapping).find(e => ua.includes(e));
